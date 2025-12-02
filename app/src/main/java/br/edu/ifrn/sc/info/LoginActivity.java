33d91928d7e180898a3040cbfe622415a36e25ba
pinheiro-joao <o.pinheiro@escolar.ifrn.edu.br>
2025-12-02 17:52:54 -0300

    Anotações no codigo do LoginAct, tirei aql barra preta q ficava em cima das telas no celular.

--- app/src/main/java/br/edu/ifrn/sc/info/LoginActivity.java
+++ app/src/main/java/br/edu/ifrn/sc/info/LoginActivity.java
@@ -11,57 +11,73 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 public class LoginActivity extends AppCompatActivity {
 
+    //Iniciação dos componentes da UI e config da autenticação
     private EditText etEmail, etSenha;
     private Button btnLogin, btnRegistrar;
-    private FirebaseAuth auth;
+    private FirebaseAuth auth; //Variável para interagir com o Firebase
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        super.onCreate(savedInstanceState); //Chama a implementação do metodo da classe pai (AppCompactActivity)
 
-        auth = FirebaseAuth.getInstance();
+        setContentView(R.layout.activity_login);//Define qual arquivo xml vai abrir
 
+        auth = FirebaseAuth.getInstance();//Obtem a instancia principal do Firebase
+
+        //Associa as variaveis aos compoentes pelo ID
         etEmail = findViewById(R.id.etEmail);
         etSenha = findViewById(R.id.etSenha);
         btnLogin = findViewById(R.id.btnLogin);
         btnRegistrar = findViewById(R.id.btnRegistrar);
 
+        //Verifica se ja esta logado
         FirebaseUser currentUser = auth.getCurrentUser();
+
+        //Se não for nulo, inicia o MainActivity e finaliza essa tela (finish();)
         if (currentUser != null) {
             startActivity(new Intent(this, MainActivity.class));
             finish();
         }
+
+        //Quando esses botões forem clicados as respectivas funções vão ser executadas
         btnLogin.setOnClickListener(v -> login());
         btnRegistrar.setOnClickListener(v -> registrar());
     }
 
     private void login() {
+        //Pega oq foi escrito nos campos
         String email = etEmail.getText().toString();
         String senha = etSenha.getText().toString();
 
+        //Verifica se algum campo está vazio (Empty), se tiver vai mostrar uma mensagem curta (Toast)
         if (TextUtils.isEmpty(email) || TextUtils.isEmpty(senha)) {
             Toast.makeText(this, "Preencha todos os campos", Toast.LENGTH_SHORT).show();
             return;
         }
 
+        //Tenta fazer o login, o listener Success excuta se deu crt e abre o mainActivity, se falhar mostra um Toast
         auth.signInWithEmailAndPassword(email, senha)
                 .addOnSuccessListener(r -> {
                     startActivity(new Intent(this, MainActivity.class));
+                    String email1 = r.getUser().getEmail();
+                    Toast.makeText(this, email1, Toast.LENGTH_SHORT).show();
                     finish();
                 })
                 .addOnFailureListener(e -> Toast.makeText(this, "Erro: " + e.getMessage(), Toast.LENGTH_SHORT).show());
     }
 
     private void registrar() {
+        //Pega oq foi escrito nos campos
         String email = etEmail.getText().toString();
         String senha = etSenha.getText().toString();
 
+        //Verifica se algum campo está vazio (Empty), se tiver vai mostrar uma mensagem curta (Toast)
         if (TextUtils.isEmpty(email) || TextUtils.isEmpty(senha)) {
             Toast.makeText(this, "Preencha todos os campos", Toast.LENGTH_SHORT).show();
             return;
         }
 
+        //Criação de um usuário no Firebase enviando os dados pra la, o SuccessListener é executado se deu crt.
         auth.createUserWithEmailAndPassword(email, senha)
                 .addOnSuccessListener(r -> {
                     Toast.makeText(this, "Registrado com sucesso!", Toast.LENGTH_SHORT).show();
@@ -69,5 +85,7 @@ public class LoginActivity extends AppCompatActivity {
                     finish();
                 })
                 .addOnFailureListener(e -> Toast.makeText(this, "Erro: " + e.getMessage(), Toast.LENGTH_LONG).show());
+
+
     }
 }
---
+++ app/src/main/java/br/edu/ifrn/sc/info/dominio/Usuario.java
@@ -0,0 +1,73 @@
+package br.edu.ifrn.sc.info.dominio;
+
+import java.util.Date;
+
+public class Usuario {
+    private String nome;
+    private String email;
+    private String senha;
+    private Date dataNasc;
+    private int cpf;
+    private boolean tipo; //Profissional = true, Paciente = false
+
+    public Usuario() {
+
+    }
+
+    public Usuario(String nome, String email, String senha, Date dataNasc, int cpf, boolean tipo) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.dataNasc = dataNasc;
+        this.cpf = cpf;
+        this.tipo = tipo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Date getDataNasc() {
+        return dataNasc;
+    }
+
+    public void setDataNasc(Date dataNasc) {
+        this.dataNasc = dataNasc;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public boolean isTipo() {
+        return tipo;
+    }
+
+    public void setTipo(boolean tipo) {
+        this.tipo = tipo;
+    }
+}
